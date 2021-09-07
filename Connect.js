206b52e4f59382ca4f752e3a39af44ebcba0b3ff
FrostyAnimations126 <39423395+Frostyanimations126@users.noreply.github.com>
2021-09-07 13:43:31 -0400

    Added Donate.html

--- Connect.js
+++ Connect.js
@@ -1,5 +1,5 @@
 window.nWallet = function(name,currency,callback){
-			window.nWalletCallback=callback||function(){};
-			window.nWalletWindow = window.open("Wallet.html?originName="+name+"&connect="+window.location+"&currency="+currency,"blank",{width:"360px",height:"500px"})
-			window.addEventListener('message',event=>{window.nWalletWindow.close();window.nWalletResult=event.data;window.nWalletCallback(event.data)})
-		}
+	window.nWalletCallback=callback||function(){};
+	window.nWalletWindow = window.open("Wallet.html?originName="+name+"&connect="+window.location+"&currency="+currency,"blank",{width:"360px",height:"500px"})
+	window.addEventListener('message',event=>{window.nWalletWindow.close();window.nWalletResult=event.data;window.nWalletCallback(event.data)})
+}
